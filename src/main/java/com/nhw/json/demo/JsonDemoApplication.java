e28a5e7c70cf7d34ffc809b958cf86e36a9e0ae6
nHw <walat.przemyslaw@gmail.com>
2020-02-03 21:02:59 +0100

    Minor cleanup

--- src/main/java/com/nhw/json/demo/JsonDemoApplication.java
+++ src/main/java/com/nhw/json/demo/JsonDemoApplication.java
@@ -2,9 +2,7 @@ package com.nhw.json.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 @Configuration
